c88e1c23512a242662f6f270d862ccad6f507359
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2024-07-15 19:34:12 -0300

    Delete backend_01 directory

--- backend_01/src/router.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const clienteController = require('./controllers/clienteControllers')
-const router = express.Router();
-
-router.get('/clientes', clienteController.getAll);
-
-module.exports = router;
